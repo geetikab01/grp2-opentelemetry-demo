3718dd26508bc6101f705d46a0a7054d01bf085c
Geetika Babu <168034834+geetikab01@users.noreply.github.com>
2025-05-09 18:29:59 -0400

    Initial commit for Phase1 deployment

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,1 +1,59 @@
+resource "aws_security_group" "instance_sg" {
+  name        = "${var.project_name}-instance-sg"
+  description = "Security group for EC2 instances"
+  vpc_id      = var.vpc_id
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH access"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP access"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS access"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "${var.project_name}-instance-sg"
+  }
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.public_subnet_ids[0]
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+  user_data              = var.user_data
+
+  root_block_device {
+    volume_size = var.volume_size
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "${var.project_name}-instance"
+  }
+}
